1fb35228664fe29a42e803fdeca48a000aa289e9
Diego Villacís <diegovillacis101@gmail.com>
2017-06-04 12:11:01 -0500

    Adding two buttons with no function yet

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
+import { Alert, Button, Text, View } from 'react-native';
+
+const onButtonPress = () => {
+  Alert.alert('Button has been pressed!');
+};
 
 const Counter = ({ value }) => (
-  <View>
-    <Text>{value}</Text>
+  <View style={{
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }}>
+    <Text style={{ fontSize: 30 }}>{value}</Text>
+    <View style={{ justifyContent: 'space-between', flexDirection: 'row'}}>
+      <Button
+        onPress={onButtonPress}
+        title='+'
+        color='#2196F3'
+      />
+      <Button
+        onPress={onButtonPress}
+        title='-'
+        color='#2196F3'
+      />
+    </View>
+
   </View>
 );
 
